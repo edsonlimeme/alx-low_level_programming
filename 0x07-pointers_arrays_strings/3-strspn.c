d7534f45ad3ec38ecf871e5825b287a60a12b151
edsonlimeme <ed.limeme@gmail.com>
2023-04-03 07:59:26 -0700

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int b = 0;
+	int y;
+
+	while (*s)
+	{
+		for (y = 0; accept[r]; y++)
+		{
+			if (*s == accept[y])
+			{
+				b++;
+				break;
+			}
+			else if (accept[y + 1] == '\0')
+				return (b);
+		}
+		s++;
+	}
+	return (b);
+}
