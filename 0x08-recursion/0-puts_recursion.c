27a70f36dd0e98a314ae025bfbe01adb5f1f83e3
edsonlimeme <ed.limeme@gmail.com>
2023-04-05 06:31:27 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *r)
+{
+	if (*r)
+	{
+		_putchar(*r);
+		_puts_recursion(r + 1);
+	}
+
+	else
+		_putchar('\n');
+}
